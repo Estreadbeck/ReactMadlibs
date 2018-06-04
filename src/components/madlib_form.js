15e5172688b056e1925a795bd0e5bcb6f0690380
Ethan Streadbeck <estreadbeck@gmail.com>
2018-06-04 13:59:58 -0600

    restructured basic app functions with restart

--- src/components/madlib_form.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class MadlibForm extends Component {
-    render() {
-      return <h1>Bottega Mad Libs Form Component</h1>;
-    }
-  }
-
-  export default MadlibForm;
